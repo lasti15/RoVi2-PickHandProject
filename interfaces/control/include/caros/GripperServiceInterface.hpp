7d45faeb08b2ce3a8248b8854697a87965a621ee
Michael Bejer-Andersen <mband@mmmi.sdu.dk>
2014-02-21 13:18:13 +0100

    Gripper service interface without support for state information from the gripper device.

---
+++ interfaces/control/include/caros/GripperServiceInterface.hpp
@@ -0,0 +1,138 @@
+#ifndef GRIPPERSERVICEINTERFACE_HPP_
+#define GRIPPERSERVICEINTERFACE_HPP_
+
+#include <rw/math/Q.hpp>
+#include <rw/common/Ptr.hpp>
+
+#include <ros/ros.h>
+#include <caros_control/GripperMoveQ.h>
+#include <caros_control/GripperGripQ.h>
+#include <caros_control/GripperSetForceQ.h>
+#include <caros_control/GripperSetVelocityQ.h>
+#include <caros_control/GripperStopMovement.h>
+
+#include <string>
+
+/**
+ * @brief This is the gripper interface. It defines the
+ * minimum interface that a configuration based robotic grasping device needs
+ * to implement.
+ *
+ * In ROS the namespace of the node is used and it is important that
+ * not two GripperServiceInterfaces are running in the same namespace.
+ *
+ */
+class GripperServiceInterface {
+public:
+
+  /**
+   * @brief constructor.
+   * @param nodehandle [in] the nodehandle to use for services.
+   */
+  GripperServiceInterface(rw::common::Ptr<ros::NodeHandle> nodehandle);
+
+  /**
+   * @brief constructor
+   * @param service_name [in] the namespace used for creating a nodehandle.
+   */
+  GripperServiceInterface(const std::string& service_name);
+
+  /**
+   * @brief virtual destructor
+   */
+  virtual ~GripperServiceInterface();
+
+  /**
+   * @brief signal the gripper to move into a specific configuration Q.
+   * @param q
+   * @return
+   */
+  virtual bool moveQ(rw::math::Q& q) = 0;
+
+  /**
+   * @brief signal the gripper to move into a specific configuration Q. The gripper will not show an error in its state if the configuration Q can not be reached.
+   * @param q
+   * @return
+   */
+  virtual bool gripQ(rw::math::Q& q) = 0;
+
+  /**
+   * @brief set the desired force configuration that the gripper should use.
+   * @param q
+   * @return
+   */
+  virtual bool setForceQ(rw::math::Q& q) = 0;
+
+  /**
+   * @brief set the desired velocity configuration that the gripper should use.
+   * @param q
+   * @return
+   */
+  virtual bool setVelocityQ(rw::math::Q& q) = 0;
+
+  /**
+   * @brief signal the gripper to stop all its movements.
+   * It should not power down the gripper and/or disconnect from the gripper.
+   * @return
+   */
+  virtual bool stopMovement(void) = 0;
+
+protected:
+
+  /**
+   * @brief publish the state of the gripper
+   * @param q [in] joint configuration
+   * @param dq [in] joint velocity
+   */
+  /*
+  void publishState(const rw::math::Q& q, const rw::math::Q& dq);
+  */
+
+private:
+  /**
+   * @brief private default constructor.
+   * This is declared as private to enforce deriving classes to call an available public constructor and enforce that the ROS services are properly initialised.
+   */
+  GripperServiceInterface();
+
+  /**
+   * @brief internal GripperServiceInterface function to properly advertise the services using ROS and setup the callbacks.
+   * This should not be called directly.
+   */
+  void initGripperService();
+
+
+  /* - these functions should be grouped together in the documentation t shortly describe that these are converting from ROS types to e.g. RobWork types according to what the interface expects - */
+  bool moveQHandle(caros_control::GripperMoveQ::Request& request,
+		     caros_control::GripperMoveQ::Response& response);
+
+  bool gripQHandle(caros_control::GripperGripQ::Request& request,
+		     caros_control::GripperGripQ::Response& response);
+
+  bool setForceQHandle(caros_control::GripperSetForceQ::Request& request,
+			 caros_control::GripperSetForceQ::Response& response);
+
+  bool setVelocityQHandle(caros_control::GripperSetVelocityQ::Request& request,
+			    caros_control::GripperSetVelocityQ::Response& response);
+
+  bool stopMovementHandle(caros_control::GripperStopMovement::Request& request,
+			    caros_control::GripperStopMovement::Response& response);
+
+private:
+  rw::common::Ptr<ros::NodeHandle> _nodeHnd;
+
+  /*
+  ros::Publisher _gripperStatePublisher;
+  */
+
+  ros::ServiceServer _srvMoveQ;
+  ros::ServiceServer _srvGripQ;
+  ros::ServiceServer _srvSetForceQ;
+  ros::ServiceServer _srvSetVelocityQ;
+  ros::ServiceServer _srvStopMovement;
+
+  std::string _gripperName;
+
+};
+
+#endif
---
+++ interfaces/control/src/GripperServiceInterface.cpp
@@ -0,0 +1,92 @@
+#include <caros/GripperServiceInterface.hpp>
+#include <ros/ros.h>
+#include <rw/common/Ptr.hpp>
+#include <string>
+
+/*
+#define QUEUE_SIZE 5
+*/
+
+GripperServiceInterface::GripperServiceInterface(const std::string& serviceName):
+  _nodeHnd( rw::common::ownedPtr(new ros::NodeHandle(serviceName) ) ),
+	_gripperName(serviceName)
+
+{
+  initGripperService();
+}
+
+
+GripperServiceInterface::GripperServiceInterface(rw::common::Ptr<ros::NodeHandle> nodehandle):
+	_nodeHnd( nodehandle ),
+	_gripperName( nodehandle->getNamespace() )
+
+{
+  initGripperService();
+}
+
+/*
+void GripperServiceInterface::publishState(const rw::math::Q& q, const rw::math::Q& dq) {
+    GripperState state;
+
+    state.header.stamp = ros::Time::now();
+
+    state.q.data.resize( q.size() );
+    state.dq.data.resize( dq.size() );
+
+    for (size_t i = 0; i<q.size(); i++) {
+    	state.q.data[i] = q(i);
+    }
+    for (size_t i = 0; i<dq.size(); i++) {
+    	state.dq.data[i] = dq[i];
+    }
+
+    _gripperStatePublisher.publish( state );
+}
+*/
+
+/** TODO
+ * - Check for errors such as if _srvMoveQ is false (see e.g. http://docs.ros.org/api/roscpp/html/classros_1_1NodeHandle.html#ae659319707eb40e8ef302763f7d632da under returns)
+ * - Properly handle the possible exception from advertise(Service) - do we want to provide e.g. exeception safety?
+ */
+
+void GripperServiceInterface::initGripperService() {
+/*
+  _gripperStatePublisher = _nodeHnd->advertise<GripperState>("GripperState", QUEUE_SIZE);
+*/
+
+  _srvMoveQ = _nodeHnd->advertiseService("moveQ", &GripperServiceInterface::moveQHandle, this);
+  _srvGripQ = _nodeHnd->advertiseService("gripQ", &GripperServiceInterface::gripQHandle, this);
+  _srvSetForceQ = _nodeHnd->advertiseService("setForceQ", &GripperServiceInterface::setForceQHandle, this);
+  _srvSetVelocityQ = _nodeHnd->advertiseService("setVelocityQ", &GripperServiceInterface::setVelocityQHandle, this);
+  _srvStopMovement = _nodeHnd->advertiseService("stopMovement", &GripperServiceInterface::stopMovementHandle, this);
+}
+
+bool GripperServiceInterface::moveQHandle(caros_control::GripperMoveQ::Request& request,
+					  caros_control::GripperMoveQ::Response& response)
+{
+  return moveQ(caros_common::fromRos(request.q));
+}
+
+bool GripperServiceInterface::gripQHandle(caros_control::GripperGripQ::Request& request,
+					  caros_control::GripperGripQ::Response& response)
+{
+  return gripQ(caros_common::fromRos(request.q));
+}
+
+bool GripperServiceInterface::setForceQHandle(caros_control::GripperSetForceQ::Request& request,
+					      caros_control::GripperSetForceQ::Response& response)
+{
+  return setForceQ(caros_common::fromRos(request.force));
+}
+
+bool GripperServiceInterface::setVelocityQHandle(caros_control::GripperSetVelocityQ::Request& request,
+						 caros_control::GripperSetVelocityQ::Response& response)
+{
+  return setVelocityQ(caros_common::fromRos(request.velocity));
+}
+
+bool GripperServiceInterface::stopMovementHandle(caros_control::GripperStopMovement::Request& request,
+						 caros_control::GripperStopMovement::Response& response)
+{
+  return stopMovement();
+}
