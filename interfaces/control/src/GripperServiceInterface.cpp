d845f4de4cd6e12055cab6e18b32f041e7689282
Michael Bejer-Andersen <mband@mmmi.sdu.dk>
2014-02-24 16:46:30 +0100

    Placing the GripperServiceInterface within the caros namespace.

--- interfaces/control/src/GripperServiceInterface.cpp
+++ interfaces/control/src/GripperServiceInterface.cpp
@@ -5,28 +5,30 @@
 #include <string>
 
 /*
-#define QUEUE_SIZE 5
+  #define QUEUE_SIZE 5
 */
 
-GripperServiceInterface::GripperServiceInterface(const std::string& serviceName):
-  _nodeHnd( rw::common::ownedPtr(new ros::NodeHandle(serviceName) ) ),
-	_gripperName(serviceName)
+namespace caros {
 
-{
-  initGripperService();
-}
+  GripperServiceInterface::GripperServiceInterface(const std::string& serviceName):
+    _nodeHnd( rw::common::ownedPtr(new ros::NodeHandle(serviceName) ) ),
+    _gripperName(serviceName)
+
+  {
+    initGripperService();
+  }
 
 
-GripperServiceInterface::GripperServiceInterface(rw::common::Ptr<ros::NodeHandle> nodehandle):
-	_nodeHnd( nodehandle ),
-	_gripperName( nodehandle->getNamespace() )
+  GripperServiceInterface::GripperServiceInterface(rw::common::Ptr<ros::NodeHandle> nodehandle):
+    _nodeHnd( nodehandle ),
+    _gripperName( nodehandle->getNamespace() )
 
-{
-  initGripperService();
-}
+  {
+    initGripperService();
+  }
 
-/*
-void GripperServiceInterface::publishState(const rw::math::Q& q, const rw::math::Q& dq) {
+  /*
+    void GripperServiceInterface::publishState(const rw::math::Q& q, const rw::math::Q& dq) {
     GripperState state;
 
     state.header.stamp = ros::Time::now();
@@ -35,59 +37,61 @@ void GripperServiceInterface::publishState(const rw::math::Q& q, const rw::math:
     state.dq.data.resize( dq.size() );
 
     for (size_t i = 0; i<q.size(); i++) {
-    	state.q.data[i] = q(i);
+    state.q.data[i] = q(i);
     }
     for (size_t i = 0; i<dq.size(); i++) {
-    	state.dq.data[i] = dq[i];
+    state.dq.data[i] = dq[i];
     }
 
     _gripperStatePublisher.publish( state );
-}
-*/
+    }
+  */
 
-/** TODO
- * - Check for errors such as if _srvMoveQ is false (see e.g. http://docs.ros.org/api/roscpp/html/classros_1_1NodeHandle.html#ae659319707eb40e8ef302763f7d632da under returns)
- * - Properly handle the possible exception from advertise(Service) - do we want to provide e.g. exeception safety?
- */
+  /** TODO
+   * - Check for errors such as if _srvMoveQ is false (see e.g. http://docs.ros.org/api/roscpp/html/classros_1_1NodeHandle.html#ae659319707eb40e8ef302763f7d632da under returns)
+   * - Properly handle the possible exception from advertise(Service) - do we want to provide e.g. exeception safety?
+   */
 
-void GripperServiceInterface::initGripperService() {
-/*
-  _gripperStatePublisher = _nodeHnd->advertise<GripperState>("GripperState", QUEUE_SIZE);
-*/
+  void GripperServiceInterface::initGripperService() {
+    /*
+      _gripperStatePublisher = _nodeHnd->advertise<GripperState>("GripperState", QUEUE_SIZE);
+    */
 
-  _srvMoveQ = _nodeHnd->advertiseService("move_q", &GripperServiceInterface::moveQHandle, this);
-  _srvGripQ = _nodeHnd->advertiseService("grip_q", &GripperServiceInterface::gripQHandle, this);
-  _srvSetForceQ = _nodeHnd->advertiseService("set_force_q", &GripperServiceInterface::setForceQHandle, this);
-  _srvSetVelocityQ = _nodeHnd->advertiseService("set_velocity_q", &GripperServiceInterface::setVelocityQHandle, this);
-  _srvStopMovement = _nodeHnd->advertiseService("stop_movement", &GripperServiceInterface::stopMovementHandle, this);
-}
+    _srvMoveQ = _nodeHnd->advertiseService("move_q", &GripperServiceInterface::moveQHandle, this);
+    _srvGripQ = _nodeHnd->advertiseService("grip_q", &GripperServiceInterface::gripQHandle, this);
+    _srvSetForceQ = _nodeHnd->advertiseService("set_force_q", &GripperServiceInterface::setForceQHandle, this);
+    _srvSetVelocityQ = _nodeHnd->advertiseService("set_velocity_q", &GripperServiceInterface::setVelocityQHandle, this);
+    _srvStopMovement = _nodeHnd->advertiseService("stop_movement", &GripperServiceInterface::stopMovementHandle, this);
+  }
 
-bool GripperServiceInterface::moveQHandle(caros_control::GripperMoveQ::Request& request,
-					  caros_control::GripperMoveQ::Response& response)
-{
-  return moveQ(caros::fromRos(request.q));
-}
+  bool GripperServiceInterface::moveQHandle(caros_control::GripperMoveQ::Request& request,
+					    caros_control::GripperMoveQ::Response& response)
+  {
+    return moveQ(caros::fromRos(request.q));
+  }
 
-bool GripperServiceInterface::gripQHandle(caros_control::GripperGripQ::Request& request,
-					  caros_control::GripperGripQ::Response& response)
-{
-  return gripQ(caros::fromRos(request.q));
-}
+  bool GripperServiceInterface::gripQHandle(caros_control::GripperGripQ::Request& request,
+					    caros_control::GripperGripQ::Response& response)
+  {
+    return gripQ(caros::fromRos(request.q));
+  }
 
-bool GripperServiceInterface::setForceQHandle(caros_control::GripperSetForceQ::Request& request,
-					      caros_control::GripperSetForceQ::Response& response)
-{
-  return setForceQ(caros::fromRos(request.force));
-}
+  bool GripperServiceInterface::setForceQHandle(caros_control::GripperSetForceQ::Request& request,
+						caros_control::GripperSetForceQ::Response& response)
+  {
+    return setForceQ(caros::fromRos(request.force));
+  }
 
-bool GripperServiceInterface::setVelocityQHandle(caros_control::GripperSetVelocityQ::Request& request,
-						 caros_control::GripperSetVelocityQ::Response& response)
-{
-  return setVelocityQ(caros::fromRos(request.velocity));
-}
+  bool GripperServiceInterface::setVelocityQHandle(caros_control::GripperSetVelocityQ::Request& request,
+						   caros_control::GripperSetVelocityQ::Response& response)
+  {
+    return setVelocityQ(caros::fromRos(request.velocity));
+  }
 
-bool GripperServiceInterface::stopMovementHandle(caros_control::GripperStopMovement::Request& request,
-						 caros_control::GripperStopMovement::Response& response)
-{
-  return stopMovement();
-}
+  bool GripperServiceInterface::stopMovementHandle(caros_control::GripperStopMovement::Request& request,
+						   caros_control::GripperStopMovement::Response& response)
+  {
+    return stopMovement();
+  }
+
+} // namespace
