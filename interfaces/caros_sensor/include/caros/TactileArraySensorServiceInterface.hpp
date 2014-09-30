e83b1f852a55bd9dd4393252520845d77e93739e
jimali <jimali@mmmi.sdu.dk>
2014-09-30 14:39:58 +0200

    added more service interfaces (not complete yet)

---
+++ interfaces/caros_sensor/include/caros/TactileArraySensorServiceInterface.hpp
@@ -0,0 +1,27 @@
+/**/
+#ifndef CAMERASENSORSERVICEINTERFACE_HPP
+#define CAMERASENSORSERVICEINTERFACE_HPP
+
+#include <rw/math/Q.hpp>
+#include <rw/math/Transform3D.hpp>
+
+#include <ros/ros.h>
+#include <string>
+
+/**
+ * @brief describe the minimum interface of a Force/Torque sensing device.
+ */
+class TactileArraySensorServiceInterface {
+public:
+	TactileArraySensorServiceInterface(const std::string& service_name);
+
+protected:
+	rw::common::Ptr<ros::NodeHandle> _nodeHnd;
+
+private:
+    //ros::Publisher _FTSensorStatePublisher;
+    //ros::ServiceServer _srvMoveQ;
+
+};
+
+#endif //#ifndef SDHSERVICEINTERFACE_HPP
