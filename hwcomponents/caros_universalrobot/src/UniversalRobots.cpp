e7ef7c55441ff48e8218f1993a6ef06b51640649
mband <mband@mmmi.sdu.dk>
2015-01-05 10:00:52 +0100

    Making a more robust parameter setup and fixing a bug together with making a clear distinction between the IP of the UR and the callbackIP of the computer communicating with the UR.

--- hwcomponents/caros_universalrobot/src/UniversalRobots.cpp
+++ hwcomponents/caros_universalrobot/src/UniversalRobots.cpp
@@ -58,9 +58,16 @@ bool UniversalRobots::activateHook() {
         CAROS_FATALERROR("The parameter 'IP' was not present on the parameter server! This parameter has to be specified for this node to work properly.", URNODE_MISSING_PARAMETER);
         return false;
     }
-    std::string port;
-    if (! _nodehandle.getParam("PORT", port)) {
-        CAROS_FATALERROR("The parameter 'PORT' was not present on the parameter server! This parameter has to be specified for this node to work properly.", URNODE_MISSING_PARAMETER);
+
+    std::string callbackIP;
+    if (! _nodehandle.getParam("callbackIP", callbackIP)) {
+        CAROS_FATALERROR("The parameter 'callbackIP' was not present on the parameter server! This parameter has to be specified for this node to work properly.", URNODE_MISSING_PARAMETER);
+        return false;
+    }
+
+    std::string callbackPort;
+    if (! _nodehandle.getParam("callbackPort", callbackPort)) {
+        CAROS_FATALERROR("The parameter 'callbackPORT' was not present on the parameter server! This parameter has to be specified for this node to work properly.", URNODE_MISSING_PARAMETER);
         return false;
     }
 
@@ -107,9 +114,9 @@ bool UniversalRobots::activateHook() {
     _urrt.connect(ip, URRT_PORT);
     _ur.connect(ip, UR_PORT);
     
-    unsigned int numericPort;
+    unsigned int numericCallbackPort = 0;
     try {
-        numericPort = std::stoul(port);
+        numericCallbackPort = std::stoul(callbackPort);
     } catch (std::invalid_argument& e) {
         /* The conversion could not be performed */
         /* TODO:
@@ -123,7 +130,7 @@ bool UniversalRobots::activateHook() {
     /* TODO:
      * Does the ordering of these two start[Interface](...) functions actually matter?
      */
-    _ur.startCommunication(ip, numericPort);
+    _ur.startCommunication(callbackIP, numericCallbackPort);
     _urrt.start();
     
     /* TODO:
