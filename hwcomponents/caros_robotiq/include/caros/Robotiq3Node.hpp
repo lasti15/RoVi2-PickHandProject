fe186faf36b092558e7d7cef7bc285bfbd782720
Dirk Kraft <kraft@mmmi.sdu.dk>
2015-01-09 21:47:33 +0100

    Extensive changes to switch over to new state machine. Many things are stolen from the SDH node.

--- hwcomponents/caros_robotiq/include/caros/Robotiq3Node.hpp
+++ hwcomponents/caros_robotiq/include/caros/Robotiq3Node.hpp
@@ -2,13 +2,15 @@
 #ifndef CAROS_ROBOTIQ3NODE_HPP
 #define CAROS_ROBOTIQ3NODE_HPP
 
-#include <caros/GripperServiceInterface.hpp>
 #include <caros/CarosNodeServiceInterface.hpp>
+#include <caros/GripperServiceInterface.hpp>
 
 #include <rwhw/robotiq/Robotiq3.hpp>
 
 #include <rw/math/Transform3D.hpp>
 #include <rw/math/Q.hpp>
+
+#include <ros/ros.h>
 
 /**
  * @brief Ros node for controlling Robotiq-3 gripper.
@@ -21,10 +23,10 @@ public:
     };
 
     //! constructor
-    Robotiq3Node(const std::string& name);
+    Robotiq3Node(const ros::NodeHandle& nodehandle);
 
     //! destructor
-    virtual ~Robotiq3Node(){};
+    virtual ~Robotiq3Node();
 
     //! @copydoc caros::GripperServiceInterface::moveQ
     bool moveQ(const rw::math::Q& q);
@@ -41,15 +43,25 @@ public:
     //! @copydoc caros::GripperServiceInterface::stopMovement
     bool stopMovement(void);
 
+    /* TODO: Properly document the error codes */
+    /* TODO: Consider better error codes for ROBOTIQ3NODE_INTERNAL_ERROR */
+    /* The enum order should not be changed, as recorded ROS sessions would then be invalidated */
+    enum ROBOTIQ3NODE_ERRORCODE { ROBOTIQ3NODE_ROBOTIQ_DEVICE_ALREADY_ACTIVE = 1, ROBOTIQ3NODE_CAROS_GRIPPER_SERVICE_CONFIGURE_FAIL, ROBOTIQ3NODE_UNSUPPORTED_INTERFACE_TYPE, ROBOTIQ3NODE_ROBOTIQ_DEVICE_CONNECT_FAILED, ROBOTIQ3NODE_INTERNAL_ERROR, ROBOTIQ3NODE_ROBOTIQ_DEVICE_NO_CONNECTION, ROBOTIQ3NODE_NO_ROBOTIQ_DEVICE, ROBOTIQ3NODE_UNSUPPORTED_Q_LENGTH };
+
+
 protected:
-    // hooks implemented from base class
+    // hooks implemented from CarosNodeServiceInterface base class
+    bool activateHook();
+    bool recoverHook();
 
     void runLoopHook();
-    bool configureHook();
-    bool cleanupHook();
-    bool startHook();
-    bool stopHook();
-    bool recoverHook();
+    void errorLoopHook();
+    void fatalErrorLoopHook();
+
+    // Utility functions to configure and connect to the Robotiq device
+    bool configureRobotiqDevice();
+    bool connectToRobotiqDevice();
+
 
 protected:
     typedef enum{MOVE,GRIP,STOP} CmdType;
@@ -58,10 +70,11 @@ protected:
     rw::math::Q _lastQ;
     std::string _ip;
     int _port;
-    std::string _nodeName;
+
 private:
     rw::common::Ptr<rwhw::Robotiq3> _robotiq;
+    ros::NodeHandle _nodeHandle;
 };
 
 
-#endif //#ifndef SDH_HPP
+#endif //#ifndef CAROS_ROBOTIQ3NODE_HPP
