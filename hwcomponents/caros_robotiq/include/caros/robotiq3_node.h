899ab7dc7f8833c7d22f49d2c18fd14434039dc5
Dirk Kraft <kraft@mmmi.sdu.dk>
2015-03-06 16:44:41 +0100

    Coding conventions commit+ - Renaming files - Changing variable names - Changing code formating - Fixing an error in caros_robotiq/include/caros/Robotiq3Node.hpp where a member was not renamed correctly in the last commit - Adding output of limits on construction and changed outcommented stuff according to new functionality

---
+++ hwcomponents/caros_robotiq/include/caros/robotiq3_node.h
@@ -0,0 +1,94 @@
+/**/
+#ifndef CAROS_ROBOTIQ_ROBOTIQ3_NODE_H
+#define CAROS_ROBOTIQ_ROBOTIQ3_NODE_H
+
+#include <caros/caros_node_service_interface.h>
+#include <caros/GripperServiceInterface.hpp>
+
+#include <rwhw/robotiq/Robotiq3.hpp>
+
+#include <rw/math/Transform3D.hpp>
+#include <rw/math/Q.hpp>
+
+#include <ros/ros.h>
+
+/**
+ * @brief Ros node for controlling Robotiq-3 gripper.
+ */
+class Robotiq3Node : public caros::CarosNodeServiceInterface, public caros::GripperServiceInterface
+{
+public:
+
+  enum ERRORCODE
+  {
+    CONNECTION_ERROR = 1 //! Connection to robotiq hand was not possible
+  };
+
+  //! constructor
+  Robotiq3Node(const ros::NodeHandle& node_handle);
+
+  //! destructor
+  virtual ~Robotiq3Node();
+
+  //! @copydoc caros::GripperServiceInterface::moveQ
+  bool moveQ(const rw::math::Q& q);
+
+  //! @copydoc caros::GripperServiceInterface::moveQ
+  bool gripQ(const rw::math::Q& q);
+
+  //! @copydoc caros::GripperServiceInterface::setForceQ
+  bool setForceQ(const rw::math::Q& q);
+
+  //! @copydoc caros::GripperServiceInterface::setVelocityQ
+  bool setVelocityQ(const rw::math::Q& q);
+
+  //! @copydoc caros::GripperServiceInterface::stopMovement
+  bool stopMovement(void);
+
+  /* TODO: Properly document the error codes */
+  /* TODO: Consider better error codes for ROBOTIQ3NODE_INTERNAL_ERROR */
+  /* The enum order should not be changed, as recorded ROS sessions would then be invalidated */
+  enum ROBOTIQ3NODE_ERRORCODE
+  {
+    ROBOTIQ3NODE_ROBOTIQ_DEVICE_ALREADY_ACTIVE = 1,
+    ROBOTIQ3NODE_CAROS_GRIPPER_SERVICE_CONFIGURE_FAIL,
+    ROBOTIQ3NODE_UNSUPPORTED_INTERFACE_TYPE,
+    ROBOTIQ3NODE_ROBOTIQ_DEVICE_CONNECT_FAILED,
+    ROBOTIQ3NODE_INTERNAL_ERROR,
+    ROBOTIQ3NODE_ROBOTIQ_DEVICE_NO_CONNECTION,
+    ROBOTIQ3NODE_NO_ROBOTIQ_DEVICE,
+    ROBOTIQ3NODE_UNSUPPORTED_Q_LENGTH
+  };
+
+protected:
+  // hooks implemented from CarosNodeServiceInterface base class
+  bool activateHook();
+  bool recoverHook();
+
+  void runLoopHook();
+  void errorLoopHook();
+  void fatalErrorLoopHook();
+
+  // Utility functions to configure and connect to the Robotiq device
+  bool configureRobotiqDevice();
+  bool connectToRobotiqDevice();
+
+protected:
+  typedef enum
+  {
+    MOVE,
+    GRIP,
+    STOP
+  } CmdType;
+  int last_cmd_;
+  ros::Time last_loop_time_;
+  rw::math::Q last_Q_;
+  std::string ip_;
+  int port_;
+
+private:
+  rw::common::Ptr<rwhw::Robotiq3> robotiq3_;
+  ros::NodeHandle node_handle_;
+};
+
+#endif //#ifndef CAROS_ROBOTIQ_ROBOTIQ3_NODE_H
---
+++ hwcomponents/caros_robotiq/src/main_robotiq3.cpp
@@ -0,0 +1,14 @@
+#include "ros/ros.h"
+#include <caros/robotiq3_node.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "robotiq3");
+
+  ros::NodeHandle nh("~");
+  std::string name = nh.getNamespace();
+  Robotiq3Node hand(name);
+  hand.start();
+
+  return 0;
+}
