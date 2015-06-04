4d7b35c4a5fbb54ec2b87f5ade08b9b4d66c99f2
mband <mband@mmmi.sdu.dk>
2015-06-04 15:49:03 +0200

    Make better simple demos or tests for the caros_universalrobot node.

---
+++ hwcomponents/caros_universalrobot/test/simple_demo_using_move_ptp.cpp
@@ -0,0 +1,22 @@
+#include "universal_robot_test.h"
+
+#include <ros/ros.h>
+
+int main(int argc, char* argv[])
+{
+  ros::init(argc, argv, "ur_simple_demo");
+
+  const double qChange = 0.2;
+
+  UrTest urTest;
+  bool ret = false;
+  ret = urTest.testMovePtp(qChange);
+
+  if (not ret)
+  {
+    ROS_ERROR_STREAM("Could not properly do the testMovePtp");
+    return 1;
+  }
+
+  return 0;
+}
